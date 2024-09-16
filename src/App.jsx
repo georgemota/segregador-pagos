c4fe53c08145bbd6a13f9e6aed72e1158971df56
George Mota <gamcustodio@outlook.com>
2024-09-16 10:56:30 -0400

    Feactures
    Feactures

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
+import './App.css';  // Importar el archivo de estilos
 
 const App = () => {
   const [personOneData, setPersonOneData] = useState([]);
   const [personTwoData, setPersonTwoData] = useState([]);
   const [history, setHistory] = useState({ personOne: [], personTwo: [] });
-  const [isHistoryVisible, setIsHistoryVisible] = useState(false);  // Estado para mostrar/ocultar historial
+  const [isHistoryVisible, setIsHistoryVisible] = useState(false);
 
   const [amountOne, setAmountOne] = useState('');
   const [descOne, setDescOne] = useState('');
+  const [dateOne, setDateOne] = useState('');
   const [amountTwo, setAmountTwo] = useState('');
   const [descTwo, setDescTwo] = useState('');
+  const [dateTwo, setDateTwo] = useState('');
 
   const handleAddOne = () => {
-    setPersonOneData([...personOneData, { amount: parseFloat(amountOne), desc: descOne }]);
+    setPersonOneData([...personOneData, { amount: parseFloat(amountOne), desc: descOne, date: dateOne }]);
     setAmountOne('');
     setDescOne('');
+    setDateOne('');
   };
 
   const handleAddTwo = () => {
-    setPersonTwoData([...personTwoData, { amount: parseFloat(amountTwo), desc: descTwo }]);
+    setPersonTwoData([...personTwoData, { amount: parseFloat(amountTwo), desc: descTwo, date: dateTwo }]);
     setAmountTwo('');
     setDescTwo('');
+    setDateTwo('');
   };
 
   const handleClear = () => {
@@ -37,129 +42,131 @@ const App = () => {
 
   const difference = Math.abs(halfOne - halfTwo);
 
-  const totalStyleOne = totalOne > totalTwo ? { color: 'green' } : { color: 'red' };
-  const totalStyleTwo = totalTwo > totalOne ? { color: 'green' } : { color: 'red' };
-
   return (
-    <div style={{ 
-      backgroundImage: `url('https://path-to-rick-and-morty-image.jpg')`,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      minHeight: '100vh',
-      padding: '20px'
-    }}>
+    <div className="main-container">
+      {/* Título centralizado */}
       <h1>Segregador de Pagos</h1>
-      
-      {/* Formulario para persona 1 */}
-      <h2>Persona 1</h2>
-      <input 
-        type="number" 
-        placeholder="Monto" 
-        value={amountOne} 
-        onChange={(e) => setAmountOne(e.target.value)} 
-      />
-      <input 
-        type="text" 
-        placeholder="Descripción" 
-        value={descOne} 
-        onChange={(e) => setDescOne(e.target.value)} 
-      />
-      <button onClick={handleAddOne}>Agregar</button>
 
-      {/* Formulario para persona 2 */}
-      <h2>Persona 2</h2>
-      <input 
-        type="number" 
-        placeholder="Monto" 
-        value={amountTwo} 
-        onChange={(e) => setAmountTwo(e.target.value)} 
-      />
-      <input 
-        type="text" 
-        placeholder="Descripción" 
-        value={descTwo} 
-        onChange={(e) => setDescTwo(e.target.value)} 
-      />
-      <button onClick={handleAddTwo}>Agregar</button>
+      <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
+        {/* Sección Izquierda */}
+        <div className="left-section">
+          <h2 style={{ color: 'white' }}>George</h2>
+          
+          {/* Formulario para George */}
+          <input 
+            type="number" 
+            placeholder="Monto" 
+            value={amountOne} 
+            onChange={(e) => setAmountOne(e.target.value)} 
+          />
+          <input 
+            type="text" 
+            placeholder="Descripción" 
+            value={descOne} 
+            onChange={(e) => setDescOne(e.target.value)} 
+          />
+          <input 
+            type="date" 
+            value={dateOne} 
+            onChange={(e) => setDateOne(e.target.value)} 
+          />
+          <button onClick={handleAddOne} className="agregar">Agregar</button>
 
-      {/* Tabla para mostrar los datos */}
-      <h3>Datos de Persona 1</h3>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Monto</th>
-            <th>Descripción</th>
-          </tr>
-        </thead>
-        <tbody>
-          {personOneData.map((item, index) => (
-            <tr key={index}>
-              <td>{item.amount}</td>
-              <td>{item.desc}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+          <h2 style={{ color: 'white' }}>Edil</h2>
+          {/* Formulario para Edil */}
+          <input 
+            type="number" 
+            placeholder="Monto" 
+            value={amountTwo} 
+            onChange={(e) => setAmountTwo(e.target.value)} 
+          />
+          <input 
+            type="text" 
+            placeholder="Descripción" 
+            value={descTwo} 
+            onChange={(e) => setDescTwo(e.target.value)} 
+          />
+          <input 
+            type="date" 
+            value={dateTwo} 
+            onChange={(e) => setDateTwo(e.target.value)} 
+          />
+          <button onClick={handleAddTwo} className="agregar">Agregar</button>
+        </div>
 
-      <h3>Datos de Persona 2</h3>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Monto</th>
-            <th>Descripción</th>
-          </tr>
-        </thead>
-        <tbody>
-          {personTwoData.map((item, index) => (
-            <tr key={index}>
-              <td>{item.amount}</td>
-              <td>{item.desc}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        {/* Sección Derecha */}
+        <div className="right-section">
+          <h3>Datos de George</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Descripción</th>
+                <th>Monto</th>
+                <th>Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              {personOneData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.desc}</td>
+                  <td>{item.amount}</td>
+                  <td>{item.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-      {/* Totales */}
-      <h2>Totales</h2>
-      <p style={totalStyleOne}>Total Persona 1: {totalOne}</p>
-      <p style={totalStyleTwo}>Total Persona 2: {totalTwo}</p>
+          <h3>Datos de Edil</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Descripción</th>
+                <th>Monto</th>
+                <th>Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              {personTwoData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.desc}</td>
+                  <td>{item.amount}</td>
+                  <td>{item.date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
 
-      <h3>Diferencia: {difference}</h3>
+          <h2>Totales</h2>
+          <p className={totalOne > totalTwo ? 'total-positive' : 'total-negative'}>Total George: {totalOne}</p>
+          <p className={totalTwo > totalOne ? 'total-positive' : 'total-negative'}>Total Edil: {totalTwo}</p>
 
-      {/* Botón para limpiar datos */}
-      <button onClick={handleClear}>Limpiar Montos</button>
+          <h3>Diferencia: {difference}</h3>
+        </div>
+      </div>
 
-      {/* Botón para mostrar/ocultar historial */}
-      <button onClick={() => setIsHistoryVisible(!isHistoryVisible)}>
-        {isHistoryVisible ? 'Ocultar Historial' : 'Mostrar Historial'}
-      </button>
+      {/* Contenedor para los botones */}
+      <div className="buttons-container">
+        <button onClick={handleClear} className="limpiar">Limpiar Montos</button>
+        <button onClick={() => setIsHistoryVisible(!isHistoryVisible)} className="historial">
+          {isHistoryVisible ? 'Ocultar Historial' : 'Mostrar Historial'}
+        </button>
+      </div>
 
       {/* Menú lateral para historial */}
       {isHistoryVisible && (
-        <div style={{
-          position: 'fixed', 
-          right: 0, 
-          top: 0, 
-          width: '250px', 
-          height: '100%', 
-          backgroundColor: 'rgba(0, 0, 0, 0.7)', 
-          color: 'white', 
-          padding: '20px', 
-          overflowY: 'auto'
-        }}>
+        <div className="historial-menu">
           <h3>Historial de la última limpieza</h3>
-          <h4>Persona 1</h4>
+          <h4>George</h4>
           <ul>
             {history.personOne.map((item, index) => (
-              <li key={index}>{item.desc}: {item.amount}</li>
+              <li key={index}>{item.desc}: {item.amount} ({item.date})</li>
             ))}
           </ul>
 
-          <h4>Persona 2</h4>
+          <h4>Edil</h4>
           <ul>
             {history.personTwo.map((item, index) => (
-              <li key={index}>{item.desc}: {item.amount}</li>
+              <li key={index}>{item.desc}: {item.amount} ({item.date})</li>
             ))}
           </ul>
         </div>
@@ -167,6 +174,5 @@ const App = () => {
     </div>
   );
 };
-
 
 export default App;
